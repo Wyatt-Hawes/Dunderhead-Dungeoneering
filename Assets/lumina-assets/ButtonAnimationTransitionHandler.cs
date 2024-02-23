b6cea62c7fbb9139440ebaea6530b1fd8d1248a3
Lumina <Luminadang@gmail.com>
2024-02-23 00:17:30 -0800

    Re-made Portrait button animations

--- Assets/lumina-assets/ButtonAnimationTransitionHandler.cs
+++ Assets/lumina-assets/ButtonAnimationTransitionHandler.cs
@@ -8,31 +8,42 @@ public class ButtonAnimationTransitionHandler : MonoBehaviour, IPointerUpHandler
     private void Start()
     {
         animator = GetComponent<Animator>();
-
-        // Subscribe to the animation event
-        AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
-        foreach (AnimationClip clip in clips)
+        if (animator != null)
         {
-            AnimationEvent animEvent = new AnimationEvent();
-            animEvent.time = clip.length; // Event time is set to the end of the animation
-            animEvent.functionName = "OnAnimationEnd"; // Call OnAnimationEnd method
-            clip.AddEvent(animEvent); // Add the event to the animation clip
+            // Subscribe to the animation event
+            AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+            foreach (AnimationClip clip in clips)
+            {
+                AnimationEvent animEvent = new AnimationEvent();
+                animEvent.time = clip.length; // Event time is set to the end of the animation
+                animEvent.functionName = "OnAnimationEnd"; // Call OnAnimationEnd method
+                clip.AddEvent(animEvent); // Add the event to the animation clip
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Animator component not found!");
         }
     }
 
     // Called when the pointer is released from the object
     public void OnPointerUp(PointerEventData eventData)
     {
-        // Set a parameter in the Animator controller to trigger the transition
-        animator.SetBool("PointerUp", true);
+        if (animator != null)
+        {
+            // Set a parameter in the Animator controller to trigger the transition
+            animator.SetBool("PointerUp", true);
+        }
     }
 
     // Called when the animation reaches its end
     private void OnAnimationEnd()
     {
-        // Reset the parameters when the animation ends
-        animator.SetBool("PointerUp", false);
-        animator.SetBool("Highlighted", false);
-
+        if (animator != null)
+        {
+            // Reset the parameters when the animation ends
+            animator.SetBool("PointerUp", false);
+            animator.SetBool("Highlighted", false);
+        }
     }
 }
